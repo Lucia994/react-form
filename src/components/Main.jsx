205c60447d9359e24dc304eea7e9368399cd52e1
Lucia994 <l.summa94@gmail.com>
2025-07-18 16:39:41 +0200

    Add list of artworks

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,42 @@
 import { useState } from "react";
 
 export default function Main() {
-  const [famousWorks, setsfamousWorks] = useState(" Infinity Mirror Room");
+  // const [famousWorks, setsfamousWorks] = useState(" Infinity Mirror Room");
+
+  const artWorks = [
+    "Infinity Mirror Room",
+    "Narcissus Garden",
+    "Dots Obsession",
+    "Pumpkin (various dates)",
+    "The Obliteration Room (2002-present)",
+    "Infinity Nets",
+    "Aggregation: One Thousand Boats Show (1963)",
+    "The Obliteration Room (2002-present)",
+    "Kusama's Self-Obliteration (1967)",
+    "Accumulation",
+  ];
+
+  // const [famousWorks, setsfamousWorks] = useState(" ");
+  // const [artWorks, setArtWorks] = useState(initila_tasks);
 
   return (
     <main>
-      <input
+      {/* <input
         type="text"
         value={famousWorks}
         onChange={(e) => setsfamousWorks(e.target.value)}
-      />
+      /> */}
+      <div className="container">
+        <ul className="list-group">
+          {artWorks.map((artwork, index) => {
+            return (
+              <li key={index} className="list-group-item">
+                {artwork}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </main>
   );
 }
