1a655df5c1016dec4a027efb09e2c812a324dc4f
Lucia994 <l.summa94@gmail.com>
2025-07-18 12:10:21 +0200

    Define the main and footer components and import them into App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
